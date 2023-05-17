882100836e31f75683cd88ccebe0a2a4b1057f48
opilane <opilane@HP-CZC2349HV4>
2023-05-17 13:56:49 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int meetodNumber = int.Parse(Console.ReadLine());
+            // sisestage soovitud meetodi number siia
+
+        if (meetodNumber == 1)
+        {
+            Meetod1();
+        }
+        else if (meetodNumber == 2)
+        {
+            Meetod2();
+        }
+        else if (meetodNumber == 3)
+        {
+            Meetod3();
+        }
+        else
+        {
+            Console.WriteLine("Vigane meetodi number!");
+        }
+    }
+
+    static void Meetod1()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string filePath = Path.Combine(desktopPath, "fail.txt");
+        File.Create(filePath);
+        Console.WriteLine("Fail loodud desktopile.");
+    }
+
+    static void Meetod2()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string filePath = Path.Combine(desktopPath, "fail.txt");
+        string fileContent = "Siia sisu kirjutada";
+        File.WriteAllText(filePath, fileContent);
+        Console.WriteLine("Fail loodud desktopile ja sisu kirjutatud.");
+    }
+
+    static void Meetod3()
+    {
+        string filePath = "ValeFailiAsukoht\\fail.txt";
+        File.Create(filePath);
+        Console.WriteLine("Fail loodud, kuid vale asukohaga.");
+    }
+}
